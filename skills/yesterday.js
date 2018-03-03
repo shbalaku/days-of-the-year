e6c1eaff7ce1fbf2580c90473f08fe687a2190d4
Shaz Balakumar <shbalaku@cisco.com>
2018-03-03 17:15:48 +0000

    caching functionality updated for yesterday.js

--- skills/yesterday.js
+++ skills/yesterday.js
@@ -12,51 +12,71 @@ module.exports = function (controller) {
 
         date = encodeYesterday();
         [date_format1, date_format2] = format_date(date);
-        year = now.getFullYear().toString();
-        uri_str = 'https://www.daysoftheyear.com/days/'+year+'/'+date;
 
-        var results = [];
-
-        request(uri_str, function(err, resp, html) {
-          if (!err){
-            var soup = new JSSoup(html);
-            var days_list = soup.findAll('h3', 'card-title');
-            var days_list2 = soup.findAll('h4', 'card-title-secondary');
-            for (var i = 0; i < days_list2.length; i++) {
-              if (((days_list2[i].text).indexOf(date_format1)>-1) || ((days_list[i].text).indexOf(date_format2)>-1)) {
-                results = results.concat(days_list[i].text);
-              }
+        cacheLookup(date_format1, function(res) {
+          if (res != 0) {
+            var date_message = "**"+date_format1+"**";
+            var output_list=date_message + '\n';
+            for (var i=0; i<res.length; i++){
+              output_list = output_list + '\n* ' + res[i];
             }
-            if (results.length == 0)
-              bot.reply(message, "Something went wrong. Sorry this happened...awkward.");
-            else {
-              // results is an array consisting of messages collected during execution
-              var date_message = "**"+date_format1+"**";
-              var output_list=date_message + '\n';
-              for (var i=0; i<results.length; i++){
-                output_list = output_list + '\n* ' + results[i];
-              }
-              bot.reply(message, output_list);
+            bot.reply(message, output_list);
+            console.log("Cache lookup successful");
+          }
+          else {
+            console.log("Cache lookup unsuccessful");
 
-              var client = createClient();
+            year = now.getFullYear().toString();
+            uri_str = 'https://www.daysoftheyear.com/days/'+year+'/'+date;
 
-              client.connect( function (err) {
-                if (err) throw err;
+            var results = [];
 
-                // execute query
-                client.query('DELETE FROM lastOutput;', function(err) {
-                  if (err) throw err;
-                  // execute addition to table query
-                  client.query('INSERT INTO lastOutput VALUES ($1, $2);', [date_format1, results], function (err) {
+            request(uri_str, function(err, resp, html) {
+              if (!err){
+                var soup = new JSSoup(html);
+                var days_list = soup.findAll('h3', 'card-title');
+                var days_list2 = soup.findAll('h4', 'card-title-secondary');
+                for (var i = 0; i < days_list2.length; i++) {
+                  if (((days_list2[i].text).indexOf(date_format1)>-1) || ((days_list[i].text).indexOf(date_format2)>-1)) {
+                    results = results.concat(days_list[i].text);
+                  }
+                }
+                if (results.length == 0)
+                  bot.reply(message, "Something went wrong. Sorry this happened...awkward.");
+                else {
+                  // results is an array consisting of messages collected during execution
+                  var date_message = "**"+date_format1+"**";
+                  var output_list=date_message + '\n';
+                  for (var i=0; i<results.length; i++){
+                    output_list = output_list + '\n* ' + results[i];
+                  }
+                  bot.reply(message, output_list);
+
+                  var client = createClient();
+
+                  client.connect( function (err) {
                     if (err) throw err;
-                    // end connection
-                    client.end( function (err) {
+
+                    // execute query
+                    client.query('DELETE FROM lastOutput;', function(err) {
                       if (err) throw err;
+                      // execute addition to table query
+                      client.query('INSERT INTO lastOutput VALUES ($1, $2);', [date_format1, results], function (err) {
+                        if (err) throw err;
+                        // execute cache insertion query
+                        client.query('INSERT INTO cache VALUES ($1, $2);', [date_format1, results], function (err) {
+                          if (err) throw err;
+                          // end connection
+                          client.end( function (err) {
+                            if (err) throw err;
+                          });
+                        });
+                      });
                     });
                   });
-                });
-              });
-            }
+                }
+              }
+            });
           }
         });
     });
@@ -110,4 +130,28 @@ function createClient() {
     ssl: true,
   });
   return client;
+}
+
+function cacheLookup(date, callback) {
+  var client = createClient();
+
+  client.connect( function(err) {
+    if (err) throw err;
+
+    // execute query
+    client.query('SELECT * FROM cache WHERE date = $1;', [date], function(err, res) {
+      if (err) throw err;
+
+      var results = [];
+      // process results
+      var row_count = res.rows.length;
+      if (row_count > 0) {
+        results = res.rows[0].days;
+        callback(results);
+      }
+      else {
+        callback(0);
+      }
+    });
+  });
 }
