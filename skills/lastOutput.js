9a006afc5548b8bc1ff453947d8a7926cf9d3edf
Shaz Balakumar <shbalaku@cisco.com>
2018-03-03 15:10:01 +0000

    last output updated

--- skills/lastOutput.js
+++ skills/lastOutput.js
@@ -8,24 +8,33 @@ const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
-client.connect();
 
 module.exports = function (controller) {
 
     controller.hears('last output', 'direct_mention, direct_message', function (bot, message) {
 
-        //client.connect();
-        client.query('SELECT * FROM lastOutput;', (err, res) => {
-          console.log(err);
-          var date = res.rows[0].date;
-          var days = res.rows[0].days;
-          var date_message = "**"+date+"**";
-          var output_list=date_message + '\n';
-          for (var i=0; i<days.length; i++){
-            output_list = output_list + '\n* ' + days[i];
-          }
-          bot.reply(message, output_list);
-          client.end();
+        client.connect(function(err) {
+            if (err) throw err;
+
+            // execute query
+            client.query('SELECT * FROM lastOutput;', function(err, res) {
+                if (err) throw err;
+
+                // process results
+                var date = res.rows[0].date;
+                var days = res.rows[0].days;
+                var date_message = "**"+date+"**";
+                var output_list=date_message + '\n';
+                for (var i=0; i<days.length; i++){
+                  output_list = output_list + '\n* ' + days[i];
+                }
+                bot.reply(message, output_list);
+                client.end(function(err) {
+                  if (err) throw err;
+                });
+              });
+            });
+          });
         });
     });
 }
