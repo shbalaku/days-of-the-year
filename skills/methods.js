c9c07ba27583d701972d722d57874d9ad91271ba
Shaz Balakumar <shbalaku@cisco.com>
2018-03-04 17:28:23 +0000

    Tomorrow test

--- skills/methods.js
+++ skills/methods.js
@@ -1,3 +1,6 @@
+var request = require('request');
+var JSSoup = require('jssoup').default;
+var now = new Date();
 const { Client } = require('pg');
 
 var methods = {
@@ -81,6 +84,69 @@ var methods = {
       if (err) throw err;
       // callback
       callback();
+    });
+  },
+  processQuery: function (date) {
+    [date1, date2] = methods.formatDate(date);
+    // look up date in cache table
+    methods.cacheLookup(date1, function(res) {
+      if (res != 0) {
+        console.log("Cache lookup successful");
+        // store last output
+        methods.storeLastOutput(date1, res, function(client) {
+          client.end(function(err) {
+            if (err) throw err;
+            var date_message = "**"+date1+"**";
+            var output_list=date_message + '\n';
+            for (var i=0; i<res.length; i++){
+              output_list = output_list + '\n* ' + res[i];
+            }
+            bot.reply(message, output_list);
+          });
+        });
+      }
+      else {
+        console.log("Cache lookup unsuccessful");
+
+        year = now.getFullYear().toString();
+        uri_str = 'https://www.daysoftheyear.com/days/'+year+'/'+date;
+
+        var results = [];
+
+        request(uri_str, function(err, resp, html) {
+          if (!err){
+            var soup = new JSSoup(html);
+            var days_list = soup.findAll('h3', 'card-title');
+            var days_list2 = soup.findAll('h4', 'card-title-secondary');
+            for (var i = 0; i < days_list2.length; i++) {
+              if (((days_list2[i].text).indexOf(date1)>-1) || ((days_list[i].text).indexOf(date2)>-1)) {
+                results = results.concat(days_list[i].text);
+              }
+            }
+            if (results.length == 0)
+              bot.reply(message, "Something went wrong. Sorry this happened...awkward.");
+            else {
+              // store last output
+              methods.storeLastOutput(date1, results, function(client) {
+                methods.storeInCache(client, date1, results, function() {
+                  // end connection
+                  client.end(function(err) {
+                    if (err) throw err;
+                    // results is an array consisting of messages collected during execution
+                    var date_message = "**"+date1+"**";
+                    var output_list=date_message + '\n';
+                    for (var i=0; i<results.length; i++){
+                      output_list = output_list + '\n* ' + results[i];
+                    }
+                    // bot reply
+                    bot.reply(message, output_list);
+                  });
+                });
+              });
+            }
+          }
+        });
+      }
     });
   }
 };
