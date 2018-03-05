decc02a2e76db71874f3d6d01077f350e1be2bf1
Shaz Balakumar <shbalaku@cisco.com>
2018-03-05 22:58:00 +0000

    reminders.js update

--- skills/reminders.js
+++ skills/reminders.js
@@ -46,7 +46,7 @@ function searchDay(query, bot, message, callback) {
         for (var i = 0; i < days_list.length; i++) {
           var res = methods.convertString(days_list[i].text).toUpperCase();
           if (res == match){
-            date = date_list[i].contents;
+            date = date_list[i].contents[0]._text;
             break;
           }
         }
