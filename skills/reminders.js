71ea9357f2c8d7a92d70c152d9dab222a9462556
Shaz Balakumar <shbalaku@cisco.com>
2018-03-05 21:36:22 +0000

    reminders.js update

--- skills/reminders.js
+++ skills/reminders.js
@@ -7,8 +7,7 @@ module.exports = function (controller) {
     controller.hears('remind (.*)', 'direct_mention, direct_message', function (bot, message) {
         var query = message.match[1];
         var personId = message.raw_message.actorId;
-        console.log(personId);
-        // process date query
-        //methods.processQuery(date, bot, message);
+        var email = message.raw_message;
+        console.log(email);
     });
 }
