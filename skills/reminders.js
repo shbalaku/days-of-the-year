1999123d5ef7c7ec5149a0b0285c9ca28ba9b9c2
Shaz Balakumar <shbalaku@cisco.com>
2018-03-06 20:53:26 +0000

    reminders.js updated

--- skills/reminders.js
+++ skills/reminders.js
@@ -86,7 +86,7 @@ function searchDay(query, bot, message, callback) {
           }
           if (matches.length > 0) {
             var match = matches[0];
-            console.log(date_list[0]);
+            console.log(date_list[0].nextElement.nextElement._text;);
             link = days_list[0].nextElement.attrs.href;
             request(link, function(_err, _resp, _html) {
               if (!_err){
