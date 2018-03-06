9b8c79079271b84d583a8769e31a9ce1ef064e1a
Shaz Balakumar <shbalaku@cisco.com>
2018-03-06 20:10:09 +0000

    help updated

--- skills/help.js
+++ skills/help.js
@@ -6,16 +6,16 @@ module.exports = function (controller) {
     controller.hears('help', 'direct_message,direct_mention', function (bot, message) {
         var text = '**Help**\n* Format of command: <dd><mm>\n* Example: 01/17';
         bot.reply(message, text);
-        /*var text = "Here are my skills:";
-        text += "\n- " + bot.appendMention(message, "color") + ": ask to pick a random color";
-        text += "\n- " + bot.appendMention(message, "restricted") + ": let a user pick a color among a set of options";
-        text += "\n- " + bot.appendMention(message, "storage") + ": store picked color as a user preference";
-        text += "\n- " + bot.appendMention(message, "threads") + ": branch to another thread";
-        text += "\n- " + bot.appendMention(message, "variables") + ": enriched user-context among threads";
+        var text = "Here are my skills:";
+        text += "\n- " + bot.appendMention(message, "today") + ": ask for list of today's events";
+        text += "\n- " + bot.appendMention(message, "tomorrow") + ": ask for list of tomorrow's events";
+        text += "\n- " + bot.appendMention(message, "yesterday") + ": ask for list of yesterday's events";
+        text += "\n- " + bot.appendMention(message, "<dd><mm>") + ": ask for list of events on <dd><mm>";
+        text += "\n- " + bot.appendMention(message, "when is <day>") + ": ask when <day> is";
+        text += "\n- " + bot.appendMention(message, "remind <day>") + ": ask to remind you of <day>";
         text += "\n\nI also understand:";
         text += "\n- " + bot.appendMention(message, "about") + ": shows metadata about myself";
         text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
-        text += "\n- " + bot.appendMention(message, "show [skill]") + ": display the code of the specified skill";
-        bot.reply(message, text);*/
+        bot.reply(message, text);
     });
 }
