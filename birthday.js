4bf63a3b7b51de494ef41a532e0b58677c9c0fe9
Shaz Balakumar <shbalaku@cisco.com>
2018-03-08 11:38:25 +0000

    birthday.js

--- birthday.js
+++ birthday.js
@@ -4,7 +4,7 @@ var spark = new CiscoSpark(process.env.SPARK_TOKEN);
 var text = "Happy birthday Max!";
 
 spark.messages.create({
-  toPersonEmail: "mwybrow@cisco.com"
+  toPersonEmail: "mwybrow@cisco.com",
   text: text
 }, function (err, result) {
   if (err) console.error(err);
