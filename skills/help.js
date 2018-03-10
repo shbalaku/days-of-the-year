c1f0e9ce39b3eb62d30c8856a45bfbc3b16d0d16
Shaz Balakumar <shbalaku@cisco.com>
2018-03-10 16:12:32 +0000

    test

--- skills/help.js
+++ skills/help.js
@@ -8,9 +8,9 @@ module.exports = function (controller) {
         text += "\n- " + bot.appendMention(message, "today") + ": ask for list of today's events";
         text += "\n- " + bot.appendMention(message, "tomorrow") + ": ask for list of tomorrow's events";
         text += "\n- " + bot.appendMention(message, "yesterday") + ": ask for list of yesterday's events";
-        text += "\n- " + bot.appendMention(message, "<dd>/<mm>") + ": ask for list of events on __dd/mm__";
-        text += "\n- " + bot.appendMention(message, "when is <day>") + ": ask when __day__ is";
-        text += "\n- " + bot.appendMention(message, "remind <day>") + ": ask to remind you of __day__";
+        text += "\n- " + bot.appendMention(message, "<dd>/<mm>") + ": ask for list of events on _dd/mm_";
+        text += "\n- " + bot.appendMention(message, "when is <day>") + ": ask when a certain day is";
+        text += "\n- " + bot.appendMention(message, "remind <day>") + ": ask to remind you of a certain day";
         text += "\n\nI also understand:";
         text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
         bot.reply(message, text);
