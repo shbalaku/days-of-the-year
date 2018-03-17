981ee85bb431f820fd40faa7ced9b03543b6de10
Shaz Balakumar <shbalaku@cisco.com>
2018-03-17 13:20:43 +0000

    restructure

--- methods.js
+++ methods.js
@@ -208,6 +208,199 @@ var methods = {
     d = mon + "/" + day;
 
     return d;
+  },
+  searchDay: function(query, bot, message, callback) {
+    var query_encode = encodeURI(query);
+    var uri_str = 'https://www.daysoftheyear.com/search/'+query_encode+'/';
+    // request html of day page
+    request(uri_str, function(err, resp, html) {
+      if (!err){
+        var match = query.toUpperCase();
+        var soup = new JSSoup(html);
+        var days_list = soup.findAll('h3', 'card-title');
+        var date_list = soup.findAll('h4', 'card-title-secondary');
+        if (days_list.length > 0){
+          var date = '';
+          var day;
+          for (var i = 0; i < days_list.length; i++) {
+            var res = methods.convertString(days_list[i].text).toUpperCase();
+            if (res == match){
+              date = date_list[i].contents[0].nextElement.nextElement._text;
+              day = days_list[i].text;
+              break;
+            }
+          }
+          if (date != ''){
+            // exact match found
+            callback(date.trim(), day);
+          }
+          else {
+            // no exact match found so retrieve first similar result from search - to be added
+            var query_split = query.split(" ");
+            var list = [];
+            for (var i = 0; i < days_list.length; i++) {
+              list[i] = days_list[i].text;
+            }
+            for (var i = 0; i < query_split.length - 1; i++) {
+              var word_i = query_split[i];
+              var results = fuzzy.filter(word_i, list);
+              var matches = results.map(function(el) { return el.string; });
+              if (matches != [])
+                break;
+            }
+            if (matches.length > 0) {
+              var day = matches[0];
+              var date = date_list[0].previousElement.nextElement.contents[0]._text;
+              callback(date,day);
+            }
+            else {
+              bot.reply(message, "It seems the day you've tried to search for could not be found.");
+            }
+          }
+        }
+        else{
+          bot.reply(message, "It seems the day you've tried to search for could not be found.");
+        }
+      }
+    });
+  },
+  findExactMatch: function(query, callback) {
+    var client = methods.createClient();
+    client.connect(function(err) {
+      if (err) throw err;
+      // execute exact match query (case insensitive)
+      client.query('SELECT * FROM cache WHERE TEXT(days) ~* ($1);',[query], function(err,res){
+        if (err) throw err;
+        // end client connection
+        client.end(function(err) {
+          if (err) throw err;
+          // process result
+          if (res.rowCount > 0){
+            var day;
+            var date = res.rows[0].date;
+            var days_list = res.rows[0].days;
+            var regex = new RegExp(query, 'i');
+            // find day in days list returned
+            for (var i = 0; i < days_list.length; i++) {
+              if (days_list[i].match(regex))
+                day = days_list[i];
+            }
+            var resp = {
+              date: date,
+              day: day
+            };
+            callback(resp);
+          }
+          else {
+            callback(0);
+          }
+        });
+      });
+    });
+  },
+  websiteSearch: function(query, callback) {
+    var query_encode = encodeURI(query);
+    uri_str = 'https://www.daysoftheyear.com/search/'+query_encode+'/';
+    request(uri_str, function(err, resp, html) {
+      if (!err){
+        var match = query.toUpperCase();
+        var soup = new JSSoup(html);
+        var days_list = soup.findAll('h3', 'card-title');
+        if (days_list.length > 0){
+          var link = '';
+          for (var i = 0; i < days_list.length; i++) {
+            var res = methods.convertString(days_list[i].text).toUpperCase();
+            if (res == match){
+              link = days_list[i].nextElement.attrs.href;
+              break;
+            }
+          }
+          if (link != ''){
+            // exact match found
+            var result;
+            request(link, function(_err, _resp, _html) {
+              if (!_err){
+                var _soup = new JSSoup(_html);
+                var date = _soup.find('div', 'banner__title banner__title-small');
+                var day_message = _soup.find('h1', 'banner__title');
+                result = date.text + ' ' + day_message.text;
+                callback(result);
+              }
+            });
+          }
+          else {
+            // no exact match found
+            var query_split = query.split(" ");
+            var list = [];
+            for (var i = 0; i < days_list.length; i++) {
+              list[i] = days_list[i].text;
+            }
+            for (var i = 0; i < query_split.length - 1; i++) {
+              var word_i = query_split[i];
+              var results = fuzzy.filter(word_i, list);
+              var matches = results.map(function(el) { return el.string; });
+              if (matches != [])
+                break;
+            }
+
+            // bot reply all matches found
+            var response = "**I found the following matching day(s):**\n\n";
+            var size = matches.length;
+            var completed_requests = 0;
+            if (size > 0) {
+              for (var i = 0; i < size; i++) {
+                link = days_list[i].nextElement.attrs.href;
+                methods.GetMatchAttributes(link, function(result) {
+                  completed_requests++;
+                  response = response + result;
+                  if (completed_requests == size) {
+                    callback(response);
+                  }
+                });
+              }
+            }
+            else {
+              callback("It seems the day you've tried to search for could not be found.");
+            }
+          }
+        }
+        else{
+          callback("It seems the day you've tried to search for could not be found.");
+        }
+      }
+    });
+  },
+  GetMatchAttributes: function(link, callback) {
+    request(link, function(_err, _resp, _html) {
+      if (!_err){
+        var _soup = new JSSoup(_html);
+        var date = _soup.find('div', 'banner__title banner__title-small');
+        var day_message = _soup.find('h1', 'banner__title');
+        var result = "\n" + date.text + ' ' + day_message.text + "\n";
+        callback(result);
+      }
+    });
+  },
+  validateDay: function(day){
+    var day_num = parseInt(day);
+    if (day_num<32 && day_num>0){
+      return true;
+    }
+    return false;
+  },
+  validateMonth: function(month){
+    var month_num = parseInt(month);
+    if (month_num<13 && month_num > 0){
+      return true;
+    }
+    return false;
+  },
+  reformat: function(str){
+    str_num = parseInt(str);
+    if (str_num<10){
+      str = '0'+str_num.toString();
+    }
+    return str;
   }
 };
 
---
+++ skills/main.js
@@ -0,0 +1,218 @@
+//
+// Main script
+//
+var methods = require('./../methods.js');
+var chrono = require('chrono-node');
+
+//
+// Command: help
+//
+module.exports = function (controller) {
+
+    controller.hears('help', 'direct_message,direct_mention', function (bot, message) {
+        var text = "Here are my skills:";
+        text += "\n- Ask me about any day for list of events on that day e.g. what is happening on March 17th?, two days from now, today, tomorrow";
+        text += "\n- " + bot.appendMention(message, "when is <day>") + ": ask about when a certain day is, e.g. when is pina colada day";
+        text += "\n- " + bot.appendMention(message, "remind <day>") + ": ask me to remind about a certain day e.g. remind pina colada day";
+        text += "\n\nI also understand:";
+        text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
+        bot.reply(message, text);
+    });
+}
+
+//
+// Retrive last output functionality
+//
+
+module.exports = function (controller) {
+
+    controller.hears('last output', 'direct_mention, direct_message', function (bot, message) {
+
+      // Establish client POSTGRESQL
+      var client = methods.createClient();
+
+      client.connect(function(err) {
+        if (err) throw err;
+
+        // execute query
+        client.query('SELECT * FROM lastOutput;', function(err, res) {
+            if (err) throw err;
+
+            client.end(function(err) {
+              if (err) throw err;
+            });
+
+            // process results
+            var date = res.rows[0].date;
+            var days = res.rows[0].days;
+            var date_message = "**"+date+"**";
+            var output_list=date_message + '\n';
+            for (var i=0; i<days.length; i++){
+              output_list = output_list + '\n* ' + days[i];
+            }
+            bot.reply(message, output_list);
+
+          });
+        });
+      });
+}
+
+//
+// Reminders functionality
+//
+
+module.exports = function (controller) {
+
+    controller.hears('remind (.*)', 'direct_mention, direct_message', function (bot, message) {
+        var query = message.match[1];
+        //var personId = message.raw_message.actorId;
+        var email = message.raw_message.data.personEmail;
+
+        methods.searchDay(query, bot, message, function(date, day) {
+          var client = methods.createClient();
+          client.connect(function(err) {
+            if (err) throw err;
+            client.query('SELECT * FROM reminders WHERE email = $1 AND day = $2;', [email, day], function(err, res) {
+              if (err) throw err;
+              if (res.rows.length == 0){
+                client.query('INSERT INTO reminders VALUES ($1, $2, $3);', [email, date, day], function(err) {
+                  if (err) throw err;
+                  client.end(function(err) {
+                    if (err) throw err;
+                    var text = "You will be reminded about " + day + " on " + date + ".";
+                    bot.reply(message, text);
+                  });
+                });
+              }
+              else {
+                client.end(function(err) {
+                  if (err) throw err;
+                  var text = "You already have a reminder set for " + day + " on " + date + ".";
+                  bot.reply(message, text);
+                });
+              }
+            });
+          });
+        });
+    });
+}
+
+//
+// Search functionality
+//
+
+module.exports = function (controller) {
+
+    controller.hears('when is (.*)', 'direct_mention, direct_message', function (bot, message) {
+
+        //var results = [];
+        var query = message.match[1];
+        // try find exact match
+        methods.findExactMatch(query, function(res){
+          if (res != 0) {
+            console.log('Exact match found');
+            var text = res.date + ' will be ' + res.day + '!';
+            bot.reply(message, text);
+          }
+          else {
+            console.log('Could not find exact match');
+            // website search
+            methods.websiteSearch(query, function(text) {
+              bot.reply(message, text);
+            });
+          }
+        });
+    });
+}
+
+//
+// Retrive cache functionality
+//
+
+module.exports = function (controller) {
+
+    controller.hears('cache (.*)', 'direct_mention, direct_message', function (bot, message) {
+
+      var query = message.match[1];
+
+      // Establish client POSTGRESQL
+      var client = methods.createClient();
+
+      client.connect(function(err) {
+        if (err) throw err;
+
+        // execute query
+        var value = [query];
+        client.query('SELECT * FROM cache WHERE days @> $1;', [value], function(err, res) {
+            if (err) throw err;
+
+            // process results
+            var row_count = res.rows.length;
+            if (row_count > 0) {
+              var date = res.rows[0].date;
+              var output_list = query + " is celebrated on " + "**"+date+"**";
+              // end connection
+              client.end(function(err) {
+                if (err) throw err;
+                bot.reply(message, output_list);
+              });
+            }
+            else {
+              client.query('SELECT * FROM cache WHERE date = $1;', [query], function(err, res) {
+                var row_count = res.rows.length;
+                if (row_count > 0) {
+                  var date = res.rows[0].date;
+                  var days = res.rows[0].days;
+                  var date_message = "**"+date+"**";
+                  var output_list=date_message + '\n';
+                  for (var i=0; i<days.length; i++){
+                    output_list = output_list + '\n* ' + days[i];
+                  }
+                  // end connection
+                  client.end(function(err) {
+                    if (err) throw err;
+                    bot.reply(message, output_list);
+                  });
+                }
+                else {
+                  // end connection
+                  client.end(function(err) {
+                    if (err) throw err;
+                    bot.reply(message, "It seems the day you've tried to search for could not be found in the cache.");
+                  });
+                }
+              });
+            }
+          });
+        });
+      });
+}
+
+//
+// Main days functionality
+//
+
+module.exports = function (controller) {
+
+    controller.hears('(.*)', 'direct_mention, direct_message', function (bot, message) {
+      // nlp parsing
+        var chrono_obj = chrono.parse(message.text)[0];
+        console.log(chrono_obj);
+        if (chrono_obj != undefined) {
+          var day = chrono_obj.start.knownValues.day;
+          var month = chrono_obj.start.knownValues.month;
+
+          if (methods.validateDay(day) && methods.validateMonth(month)){
+            month = methods.reformat(month);
+            day = methods.reformat(day);
+            date = month + "/" + day;
+            methods.processQuery(date, function(text){
+              bot.reply(message, text);
+            });
+          }
+          else {
+            bot.reply(message, "Something went wrong. Please check you have entered a valid date. Thank you. Sorry this happened...awkward.");
+          }
+        }
+    });
+}
